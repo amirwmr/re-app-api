ca559c70aedf738dcd7ab762b8bb40b4b378c8f0
Amirwmr <amirwmr@yahoo.com>
2023-06-17 14:03:53 +0330

    Email normalized added to user model

--- app/core/models.py
+++ app/core/models.py
@@ -2,7 +2,7 @@
 Database models.
 """
 from django.db import models
-from django.contrib.auth.models import(
+from django.contrib.auth.models import (
     AbstractBaseUser,
     BaseUserManager,
     PermissionsMixin
@@ -14,11 +14,12 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         # Create, save and return a new user.
-        user = self.model(email=email, **extra_fields)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     # User in the system.
